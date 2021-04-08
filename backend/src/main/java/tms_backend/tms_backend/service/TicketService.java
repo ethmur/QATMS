a8327ded30bb0689fb5c797892bc5b8a64aceeb0
Ethan Murad <etmuop@gmail.com>
2021-04-08 14:58:16 -0400

    TMS-1, TMS-2, TMS-3, TMS-4, TMS-6, TMS-7, TMS-8, TMS-9, TMS-10, TMS-11, TMS-12 Add backend support for ticket CRUD functionality

---
+++ backend/src/main/java/tms_backend/tms_backend/service/TicketService.java
@@ -0,0 +1,50 @@
+package tms_backend.tms_backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import tms_backend.tms_backend.models.Ticket;
+import tms_backend.tms_backend.repository.TicketRepository;
+
+@Service
+public class TicketService {
+	
+	private static final Logger LOGGER = LogManager.getLogger();
+	
+	@Autowired
+	private TicketRepository ticketRespository;
+		
+	public List<Ticket> getAll() {
+		LOGGER.info("Getting all tickets");
+		return this.ticketRespository.findAll();
+	}
+	
+	public Ticket create(Ticket ticket) {
+		LOGGER.info("Creating following ticket: " + ticket);
+		return this.ticketRespository.save(ticket);
+	}
+	
+	public Ticket update(int id, Ticket ticket) {
+		LOGGER.info("Updating following ticket: " + ticket);
+		Optional<Ticket> existingOptional = this.ticketRespository.findById(id);
+        Ticket existing = existingOptional.get();
+        existing.setTitle(ticket.getAuthor());
+        existing.setDescription(ticket.getDescription());
+        existing.setAuthor(ticket.getAuthor());
+        existing.setStatus(ticket.getStatus());
+        return this.ticketRespository.save(existing);
+	}
+	
+	public boolean delete(int id) {
+		LOGGER.info("Deleting ticket with id=" + id);
+        this.ticketRespository.deleteById(id);
+        boolean exists = this.ticketRespository.existsById(id);
+        return !exists;
+	}
+
+}
